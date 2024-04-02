47d78c84646b103bc3d823a364adb080d438a424
Scarbonell11 <chebastroto@gmail.com>
2024-04-02 20:48:15 -0300

    Probando

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
+import Header from "./components/Header/Header";
+
 function App() {
   return (
     <>
       <div className="text-3xl font-bold underline">Hello world!</div>
+      <Header nombre='Maria'  />
     </>
   );
 }
---
+++ frontend/src/Header/Header.jsx
@@ -0,0 +1,13 @@
+import styles from './Header.module.css'
+
+const Header = ({nombre}) => {
+
+    
+    return (
+        <div className={styles.contenedor} >
+            <h1 className={styles.nombre} >Hola {nombre} </h1>
+        </div>
+    )
+}
+
+export default Header
