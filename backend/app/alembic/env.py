3d97a87b56e1c430896b9b65b25b4ce74e7853b8
suspiciousRaccoon <spablocabral@gmail.com>
2024-04-02 22:14:53 -0300

    refactor(models): move into own folder

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -1,11 +1,9 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
-from app.models import Base  # needs to be imported from models.py
 from app.core.config import settings
+from app.models.users import Base as UserBase
+from sqlalchemy import engine_from_config, pool
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -24,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = [UserBase.metada]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -2,4 +2,4 @@ from core.database import Base
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
 
-# models go here!
+# user models go here!
