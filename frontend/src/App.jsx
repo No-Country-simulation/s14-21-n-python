78451f0ca0ccaa30d484f02633abde4591574ab8
Scarbonell11 <chebastroto@gmail.com>
2024-04-02 21:36:17 -0300

    fix : te duplico el nombre
    pa ti

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <>
       <div className="text-3xl font-bold underline">Hello world!</div>
+      <div className="text-3xl font-bold underline">Hello world!</div>
+      <div className="text-3xl font-bold underline">Hello world!</div>
       <Header nombre='Maria'  />
     </>
   );
