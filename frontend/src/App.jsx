f31f221c89971017048a5b4a60da7c91b20a5cc1
Scarbonell11 <chebastroto@gmail.com>
2024-04-02 21:11:48 -0300

    refactor a hello world

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header/Header";
 function App() {
   return (
     <>
-      <div className="text-3xl font-bold underline">Hello world!</div>
+      <div className="text-3xl font-bold underline">Hola santi estoy haciendo las cosas bien</div>
       <Header nombre='Maria'  />
     </>
   );
