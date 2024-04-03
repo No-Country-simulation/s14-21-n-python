4ac65a9351e45d709633abcaecbdd6d6613c7dce
Scarbonell11 <chebastroto@gmail.com>
2024-04-02 21:22:06 -0300

    fix: nombre principal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header/Header";
 function App() {
   return (
     <>
-      <div className="text-3xl font-bold underline">Hola santi estoy haciendo las cosas bien</div>
+      <div className="text-3xl font-bold underline">Hello world!</div>
       <Header nombre='Maria'  />
     </>
   );
