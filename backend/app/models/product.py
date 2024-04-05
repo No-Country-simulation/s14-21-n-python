9c2fd25382666618c997bd3ec935f53b4f0c963e
suspiciousRaccoon <spablocabral@gmail.com>
2024-04-05 00:29:36 -0300

    fix(models/product.py): some wrong fields

--- backend/app/models/product.py
+++ backend/app/models/product.py
@@ -1,27 +1,25 @@
+from app.models.business import Business
+from app.models.category import Category
 from core.database import Base
+from sqlalchemy import DateTime, ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from sqlalchemy import Integer, DATETIME, DECIMAL, ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-
-#Importamos el modelo
-from models import Business, Category
 
 class Product(Base):
-    __tablename__ = 'products'
+    __tablename__ = "product"
 
     id = mapped_column(Integer, primary_key=True, increment=True)
-    business_id = mapped_column(Integer, ForeignKey('businesses.id'))
+    business_id = mapped_column(Integer, ForeignKey("businesses.id"))
     brand: Mapped[str]
     name: Mapped[str]
     barcode: Mapped[str]
     stock: Mapped[int]
     minimum_stock: Mapped[int]
-    original_price: Mapped[DECIMAL] #= mapped_column(Decimal(10, 2))
-    purchase_date: Mapped[DATETIME] #no estoy seguro
-    expiration_date: Mapped[DATETIME] #no estoy seguro
-    category_id = mapped_column(Integer, ForeignKey('categories.id'))
+    original_price: Mapped[int]
+    # https://docs.sqlalchemy.org/en/20/tutorial/metadata.html#declaring-mapped-classes can use old style for now.
+    purchase_date = mapped_column(DateTime)
+    expiration_date = mapped_column(DateTime)
+    category_id = mapped_column(Integer, ForeignKey("categories.id"))
 
-    business: Mapped["Business"] = relationship('Business', back_populates='products')
-    category: Mapped["Category"] = relationship('Category', back_populates='products')
+    business: Mapped[Business] = relationship(back_populates="products")
+    category: Mapped[Category] = relationship(back_populates="products")
