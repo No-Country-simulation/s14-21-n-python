f2b04df190e019b57c7690ea1ce8286257ecffac
suspiciousRaccoon <spablocabral@gmail.com>
2024-04-05 22:38:08 -0300

    refactor(core/database.py): use Base from DeclarativeBase

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,8 +1,5 @@
-import os
-
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.core.config import settings
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_host}:{settings.database_port}/{settings.database_name}"
@@ -10,4 +7,6 @@ SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
