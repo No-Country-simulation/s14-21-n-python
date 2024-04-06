ae638563fd7ea787262699eaf0ef43f2ceb41876
suspiciousRaccoon <spablocabral@gmail.com>
2024-04-05 22:29:28 -0300

    fix(models/product.py): wrong fields for price, added limit to str

--- backend/app/models/product.py
+++ backend/app/models/product.py
@@ -1,7 +1,7 @@
 from app.models.business import Business
 from app.models.category import Category
 from core.database import Base
-from sqlalchemy import DateTime, ForeignKey, Integer
+from sqlalchemy import DateTime, ForeignKey, Integer, Numeric, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
@@ -10,13 +10,13 @@ class Product(Base):
 
     id = mapped_column(Integer, primary_key=True, increment=True)
     business_id = mapped_column(Integer, ForeignKey("businesses.id"))
-    brand: Mapped[str]
-    name: Mapped[str]
-    barcode: Mapped[str]
+    brand: Mapped[str] = mapped_column(String(255))
+    name: Mapped[str] = mapped_column(String(255))
+    barcode: Mapped[str] = mapped_column(String(255))
     stock: Mapped[int]
-    minimum_stock: Mapped[int]
-    original_price: Mapped[int]
-    # https://docs.sqlalchemy.org/en/20/tutorial/metadata.html#declaring-mapped-classes can use old style for now.
+    # https://docs.sqlalchemy.org/en/20/tutorial/metadata.html#declaring-mapped-classes can use old style for now. we'll need to figure out the types later
+    minimum_stock = mapped_column(Numeric(10, 2))
+    original_price = mapped_column(Numeric(10, 2))
     purchase_date = mapped_column(DateTime)
     expiration_date = mapped_column(DateTime)
     category_id = mapped_column(Integer, ForeignKey("categories.id"))
