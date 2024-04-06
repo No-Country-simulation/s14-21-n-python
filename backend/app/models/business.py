4c6548093e2732c2a73cd1fd14edb405f9f845d8
suspiciousRaccoon <spablocabral@gmail.com>
2024-04-05 23:12:35 -0300

    fix(models/business.py): remove unnecesary keyword arguments

--- backend/app/models/business.py
+++ backend/app/models/business.py
@@ -3,17 +3,20 @@ from typing import Optional
 from sqlalchemy import Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-#Importamos el modelo
+# Importamos el modelo
 from models import User, Product, Transaction
 
-class Business(Base):
-    __tablename__ = 'businesses'
 
-    id = mapped_column(Integer, primary_key=True, increment=True, unique= True)
+class Business(Base):
+    __tablename__ = "business"
+
+    id = mapped_column(Integer, primary_key=True, increment=True, unique=True)
     name: Mapped[Optional[str]]
     address: Mapped[str]
     phone: Mapped[str]
 
-    users: Mapped["User"] = relationship('User', back_populates='business')
-    products: Mapped["Product"] = relationship('Product', back_populates='business')
-    transactions: Mapped["Transaction"] = relationship('Transaction', back_populates='business')
+    user: Mapped["User"] = relationship("User", back_populates="business")
+    products: Mapped["Product"] = relationship("Product", back_populates="business")
+    transactions: Mapped["Transaction"] = relationship(
+        "Transaction", back_populates="business"
+    )
