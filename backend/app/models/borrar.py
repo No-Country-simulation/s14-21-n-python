38f3eeaedc344e02d9d60f6ee368aadf419a2093
suspiciousRaccoon <spablocabral@gmail.com>
2024-04-05 23:30:06 -0300

    chore: delete file "borrar" file

--- backend/app/models/borrar.py
+++
@@ -1,8 +0,0 @@
-# models/__init__.py
-
-# from .business import Business
-# from .user import User
-# from .product import Product
-# from .supplier import Supplier
-# from .transaction import Transaction
-# from .category import Category
