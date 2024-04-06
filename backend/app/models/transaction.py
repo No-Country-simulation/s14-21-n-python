17f685ad6c73fcd4846737b5a431ae69f32b79f3
suspiciousRaccoon <spablocabral@gmail.com>
2024-04-05 23:21:18 -0300

    fix(models/transaction.py): some wrong types, left relationship stuff for tomorrow

--- backend/app/models/transaction.py
+++ backend/app/models/transaction.py
@@ -1,27 +1,29 @@
 from core.database import Base
 
-from sqlalchemy import Integer, DATETIME, DECIMAL, ForeignKey, Enum
-from sqlalchemy.orm import relationship
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+# Importamos el modelo
+from sqlalchemy import DateTime, Enum, Numeric
+from sqlalchemy.orm import Mapped, mapped_column
 
-#Importamos el modelo
-from models import Business, Supplier
 
 class Transaction(Base):
-    __tablename__ = 'transactions'
+    __tablename__ = "transaction"
 
-    id = mapped_column(Integer, primary_key=True, increment=True)
-    business_id = mapped_column(Integer, ForeignKey('businesses.id'))
-    product_id = mapped_column(Integer, ForeignKey('products.id'))
-    client_id: Mapped[int]
-    supplier_id = mapped_column(Integer, ForeignKey('suppliers.id'))
-    type: Mapped[Enum] = mapped_column(Enum('Purchase', 'Sale'))
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[Enum] = mapped_column(Enum("Purchase", "Sale"))
     quantity: Mapped[int]
-    price: Mapped[DECIMAL] #= mapped_column(Decimal(10, 2))
-    payment_method: Mapped[Enum] = mapped_column(Enum('cash', 'card', 'transfer'))
-    status: Mapped[Enum] = mapped_column(Enum('Pending', 'Approved', 'In process', 'Completed', 'Canceled'))
-    transaction_date: Mapped[DATETIME] #NO ESTOY SEGURO
+    price = mapped_column(Numeric(10, 2))
+    payment_method: Mapped[Enum] = mapped_column(Enum("cash", "card", "transfer"))
+    status: Mapped[Enum] = mapped_column(
+        Enum("Pending", "Approved", "In process", "Completed", "Canceled")
+    )
+    transaction_date = mapped_column(
+        DateTime
+    )  # NO ESTOY SEGURO # Yo tampoco, no te preocupes
 
-    business: Mapped["Business"] = relationship('Business', back_populates='transactions')
-    supplier: Mapped["Supplier"] = relationship('Supplier', back_populates='transactions')
+    # client_id: Mapped[int] # para que se usa esto?
+    # product_id = mapped_column(Integer, ForeignKey('products.id'))
+    # business_id = mapped_column(Integer, ForeignKey('businesses.id'))
+    # supplier_id = mapped_column(Integer, ForeignKey('suppliers.id'))
+
+    # business: Mapped["Business"] = relationship('Business', back_populates='transactions')
+    # supplier: Mapped["Supplier"] = relationship('Supplier', back_populates='transactions')
