08e65ffca5693466e9893ed6f60e1cf71616c2bd
suspiciousRaccoon <spablocabral@gmail.com>
2024-04-05 23:15:57 -0300

    fix(models/business.py): remove relationship stuff until tomorrow

--- backend/app/models/business.py
+++ backend/app/models/business.py
@@ -1,22 +1,27 @@
-from core.database import Base
 from typing import Optional
-from sqlalchemy import Integer
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from core.database import Base
 
 # Importamos el modelo
-from models import User, Product, Transaction
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class Business(Base):
     __tablename__ = "business"
 
-    id = mapped_column(Integer, primary_key=True, increment=True, unique=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[Optional[str]]
     address: Mapped[str]
     phone: Mapped[str]
 
-    user: Mapped["User"] = relationship("User", back_populates="business")
-    products: Mapped["Product"] = relationship("Product", back_populates="business")
-    transactions: Mapped["Transaction"] = relationship(
-        "Transaction", back_populates="business"
-    )
+    # TODO define fields
+
+    # user_id: Mapped[int] =
+    # product_id: Mapped[int] =
+    # transaction_id: Mapped[int] =
+
+    # user: Mapped["User"] = relationship("User", back_populates="business")
+    # product: Mapped["Product"] = relationship("Product", back_populates="business")
+    # transaction: Mapped["Transaction"] = relationship(
+    #     "Transaction", back_populates="business"
+    # )
