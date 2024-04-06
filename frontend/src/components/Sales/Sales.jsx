5ea2e531e4f2e26a1cd0199cdbb3f1e04598a463
Verónica <molinariveronica478@gmail.com>
2024-04-06 17:54:24 -0300

    Ventas mejoradas

--- frontend/src/components/Sales/Sales.jsx
+++ frontend/src/components/Sales/Sales.jsx
@@ -7,26 +7,66 @@ const Sales = () => {
   const testArray = [
     {
       id: "1",
-      name: "Venta 1",
       date: "fecha",
+      product: "Producto 1",
+      brand: "Marca",
+      amount: "20",
       price: "9999",
     },
     {
       id: "2",
-      name: "Venta 2",
       date: "fecha",
+      product: "Producto 2",
+      brand: "Marca",
+      amount: "20",
       price: "9999",
     },
     {
       id: "3",
-      name: "Venta 3",
       date: "fecha",
+      product: "Producto 3",
+      brand: "Marca",
+      amount: "20",
       price: "9999",
     },
     {
       id: "4",
-      name: "Venta 4",
       date: "fecha",
+      product: "Producto 4",
+      brand: "Marca",
+      amount: "20",
+      price: "9999",
+    },
+    {
+      id: "5",
+      date: "fecha",
+      product: "Producto 5",
+      brand: "Marca",
+      amount: "20",
+      price: "9999",
+    },
+    {
+      id: "6",
+      date: "fecha",
+      product: "Producto 6",
+      brand: "Marca",
+      amount: "20",
+      price: "9999",
+    },
+    {
+      id: "7",
+      date: "fecha",
+      product: "Producto 7",
+      brand: "Marca",
+      amount: "20",
+      price: "9999",
+    },
+    {
+      id: "8",
+      date: "fecha",
+      product: "Producto 8",
+      brand: "Marca",
+      amount: "20",
       price: "9999",
     },
   ];
@@ -44,24 +84,36 @@ const Sales = () => {
           <button>Generar Reporte</button>
         </div>
       </section>
-      <section className={style.layout}>
-        {testArray.map((sale) => {
-          return (
-            <div className={style.card} key={sale.id}>
-              <div className={style.data}>
-                <h3>{sale.name}</h3>
-                <p> {sale.date}</p>
-                <p> $ {sale.price} </p>
-                {modifySales && (
-                  <div>
-                    <button>Mod</button>
-                    <button>Elim</button>
-                  </div>
-                )}
+      <section>
+        <div className={style.layout}>
+          <div className={style.header}>
+            <div className={style.column}>Fecha</div>
+            <div className={style.column}>Producto</div>
+            <div className={style.column}>Marca</div>
+            <div className={style.column}>Cantidad</div>
+            <div className={style.column}>Precio</div>
+            <div className={style.column}>Administrar</div>
+          </div>
+          {testArray.map((sale) => {
+            return (
+              <div className={style.row} key={sale.id}>
+                <div className={style.column}>{sale.date}</div>
+                <div className={style.column}>{sale.product}</div>
+                <div className={style.column}>{sale.brand}</div>
+                <div className={style.column}>{sale.amount}</div>
+                <div className={style.column}>$ {sale.price}</div>
+                <div className={style.column}>
+                  {modifySales && (
+                    <div>
+                      <button>Mod</button>
+                      <button>Elim</button>
+                    </div>
+                  )}
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </section>
       <AddSale />
     </main>
