b1e8a8efe1500ef3162668b09f09977608bf477e
suspiciousRaccoon <spablocabral@gmail.com>
2024-04-08 20:50:27 -0300

    feat(models/user.py): make Business FK nullable

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,9 +1,9 @@
-from core.database import Base
 from typing import Optional
+
+from core.database import Base
+from models import Business
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-
-from models import Business
 
 
 class User(Base):
@@ -13,6 +13,6 @@ class User(Base):
     name: Mapped[Optional[str]]
     email: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
-    business_id: Mapped[int] = mapped_column(ForeignKey("business.id"))
+    business_id: Mapped[Optional["Business"]] = mapped_column(ForeignKey("business.id"))
 
-    business: Mapped["Business"] = relationship(back_populates="user")
+    business: Mapped[Optional["Business"]] = relationship(back_populates="user")
