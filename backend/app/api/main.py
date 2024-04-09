7e44b8ff8a8cd681ccd2eac1cdda364c8e1e6312
Nahuel <nahuel.94.arrieta@gmail.com>
2024-04-09 17:38:21 -0300

    fix(api/main): include api_router to auth

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-from app.api.routes import hello
+from app.api.routes import hello, auth
 
 api_router = APIRouter()
 api_router.include_router(hello.router)
+
+api_router.include_router(auth.router)
