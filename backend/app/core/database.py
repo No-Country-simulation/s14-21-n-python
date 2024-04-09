9d36e442d00d437a0a7ef2dda7d893d353afd4ee
suspiciousRaccoon <spablocabral@gmail.com>
2024-04-08 22:00:52 -0300

    fix(core/database.py): import path

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,6 +1,6 @@
+from core.config import settings
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from app.core.config import settings
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_host}:{settings.database_port}/{settings.database_name}"
 
