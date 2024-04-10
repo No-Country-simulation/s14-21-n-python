c32f4663911be73ea8a2f4bf020b7b68e2fbeb8a
suspiciousRaccoon <spablocabral@gmail.com>
2024-04-09 23:03:04 -0300

    fix(core/config.py): throw pydantic_settings into the trashcan

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,17 +1,29 @@
-from pydantic_settings import BaseSettings
+import os
+
+from dotenv import load_dotenv
+# from pydantic_settings import BaseSettings
 
 
-class Settings(BaseSettings):
-    database_password: str = "postgres"
-    database_username: str = "postgres"
-    database_host: str = "localhost"
-    database_port: str = "5432"
-    database_name: str = "postgres"
-    secret_key: str = "woops"
-    algorithm: str = "secret"
+# class Settings(BaseSettings):
+#     database_password: str = "postgres"
+#     database_username: str = "postgres"
+#     database_host: str = "localhost"
+#     database_port: str = "5432"
+#     database_name: str
+#     secret_key: str = "woops"
+#     algorithm: str = "secret"
 
-    class Config:
-        env_file = "../../.env"
+#     class Config:
+#         env_file = "../../.env"
 
+load_dotenv()
 
-settings = Settings()
+settings = {
+    "database_password": os.getenv("DATABASE_PASSWORD"),
+    "database_username": os.getenv("DATABASE_USERNAME"),
+    "database_host": os.getenv("DATABASE_HOST"),
+    "database_port": os.getenv("DATABASE_PORT"),
+    "database_name": os.getenv("DATABASE_NAME"),
+    "secret_key": os.getenv("SECRET_KEY"),
+    "algorithm": os.getenv("ALGORITHM"),
+}
