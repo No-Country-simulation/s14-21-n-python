1953e84298f3aa2f26d4e0b9d5e98e8948939028
suspiciousRaccoon <spablocabral@gmail.com>
2024-04-09 22:57:20 -0300

    fix(core/database.py): settings based on dotenv

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -2,7 +2,7 @@ from core.config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_host}:{settings.database_port}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings['database_username']}:{settings['database_password']}@{settings['database_host']}:{settings['database_port']}/{settings['database_name']}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
