d42db14bff16653a70d0976dfb1215da30de7081
wcrespo <wellingtoncrespo2001@gmail.com>
2024-04-09 22:45:03 -0500

    Agregando estilos del figma al componente orders

--- frontend/src/components/Orders/Order.jsx
+++ frontend/src/components/Orders/Order.jsx
@@ -1,132 +1,50 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./Order.module.css";
+import jsonData from "./orders.json"; 
 
 const Orders = () => {
-  const [orders, setOrders] = useState([]);
-  const [newOrder, setNewOrder] = useState("");
-  const [product, setProduct] = useState("");
-  const [quantity, setQuantity] = useState("");
-  const [provider, setProvider] = useState("");
-  const [providerList] = useState(["Proveedor 1", "Proveedor 2", "Proveedor 3"]); // Lista de proveedores
-
-  const handleAgregarPedido = (e) => {
-    e.preventDefault();
-    if (product.trim() !== "" && quantity.trim() !== "" && provider.trim() !== "") {
-      setOrders([
-        ...orders,
-        { id: orders.length + 1, product, quantity, provider, delivered: false },
-      ]);
-      setProduct("");
-      setQuantity("");
-      setProvider("");
-    }
+  const handleDeliveredOrders = () => {
+    console.log("Entragados");
   };
-
-  const handleMarkDelivered = (id) => {
-    const updatedOrders = orders.map((order) =>
-      order.id === id ? { ...order, delivered: true } : order
-    );
-    setOrders(updatedOrders);
-  };
-
-  const handleDeleteDelivered = () => {
-    const filteredOrders = orders.filter((order) => !order.delivered);
-    setOrders(filteredOrders);
-  };
-
-  const pendingOrders = orders.filter((order) => !order.delivered);
-  const deliveredOrders = orders.filter((order) => order.delivered);
 
   return (
     <div className={styles.ordersContainer}>
       <h2 className={styles.ordersTitle}>Pedidos</h2>
-      <hr className={styles.hr}/>
-      <form onSubmit={handleAgregarPedido} className={styles.inputContainer}>
-        <input
-          type="text"
-          value={product}
-          onChange={(e) => setProduct(e.target.value)}
-          placeholder="Producto"
-          className={styles.input}
-        />
-        <input
-          type="text"
-          value={quantity}
-          onChange={(e) => setQuantity(e.target.value)}
-          placeholder="Cantidad"
-          className={styles.input}
-        />
-        <select
-          value={provider}
-          onChange={(e) => setProvider(e.target.value)}
-          className={styles.select}
-        >
-          <option value="">Seleccionar Proveedor</option>
-          {providerList.map((provider, index) => (
-            <option key={index} value={provider}>
-              {provider}
-            </option>
-          ))}
-        </select>
-        <button type="submit" className={styles.button}>
-          Agregar Pedido
+      <hr className={styles.hr} />
+      <div className={styles.filterContainer}>
+        <div className={styles.inputContainer}>
+          <input
+            type="text"
+            placeholder="Filtros"
+            className={styles.filterInput}
+          />
+          <input
+            type="text"
+            placeholder="Buscar..."
+            className={styles.searchInput}
+          />
+        </div>
+        <button onClick={handleDeliveredOrders} className={styles.deliveredButton}>
+          Pedidos Entregados
         </button>
-      </form>
-      <div>
-        <h3 className={styles.subtitle}>Pedidos Pendientes</h3>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>Producto</th>
-              <th>Cantidad</th>
-              <th>Proveedor</th>
-              <th>Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {pendingOrders.map((order) => (
-              <tr key={order.id}>
-                <td>{order.product}</td>
-                <td>{order.quantity}</td>
-                <td>{order.provider}</td>
-                <td>
-                  <button
-                    onClick={() => handleMarkDelivered(order.id)}
-                    className={styles.markDelivered}
-                  >
-                    Marcar Entregado
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
       </div>
-      <div>
-        <h3 className={styles.subtitle}>Pedidos Entregados</h3>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>Producto</th>
-              <th>Cantidad</th>
-              <th>Proveedor</th>
-            </tr>
-          </thead>
-          <tbody>
-            {deliveredOrders.map((order) => (
-              <tr key={order.id}>
-                <td>{order.product}</td>
-                <td>{order.quantity}</td>
-                <td>{order.provider}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        {deliveredOrders.length > 0 && (
-          <button onClick={handleDeleteDelivered} className={styles.deleteButton}>
-            Eliminar Pedidos Entregados
-          </button>
-        )}
+      <div className={styles.table}>
+        <div className={`${styles.row} ${styles.header}`}>
+          <div className={styles.cell}>Fecha</div>
+          <div className={styles.cell}>Productos</div>
+          <div className={styles.cell}>Cantidad</div>
+          <div className={styles.cell}>Proveedor</div>
+          <div className={styles.cell}>Estado</div>
+        </div>
+        {jsonData.products.map((product, index) => (
+          <div key={index} className={`${styles.row} ${styles.rowWithMargin}`}>
+            <div className={styles.cell}>{product.date}</div>
+            <div className={styles.cell}>{product.product}</div>
+            <div className={styles.cell}>{product.amount}</div>
+            <div className={styles.cell}>{product.supplier}</div>
+            <div className={styles.cell}>{product.state[0]}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
