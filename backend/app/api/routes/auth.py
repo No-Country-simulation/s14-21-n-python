cb4321e7aae15e6435613422f509e1d981fe2d15
Nahuel <nahuel.94.arrieta@gmail.com>
2024-04-12 00:06:03 -0300

    fix(router/auth): add create_user

--- backend/app/api/routes/auth.py
+++ backend/app/api/routes/auth.py
@@ -1,17 +1,17 @@
 from typing import Annotated
 from datetime import timedelta
 from fastapi import APIRouter, Depends, status, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
-from app.schemas import Token  # <--- ver schemas
-from utils import oauth2, user as userAuth  # <--- Ver donde ubicar user
-
-from api.deps import get_db
+from sqlalchemy.orm import Session
+from app.schemas import Token, UserLogin, UserSchema  # <--- ver schemas
+from app.utils import password, oauth2, user as userAuth  # <--- Ver donde ubicar user
+from app.models.user import User
+from app.api.deps import get_db
 
 router = APIRouter(tags=["Authentication"])
 
 
-@router.post("/token")
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token:
+@router.post("/token", response_model=Token)
+async def login(form_data: Annotated[UserLogin, Depends()]) -> Token:
     user = userAuth.authenticate_user(get_db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
@@ -24,3 +24,14 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> T
         data={"sub": user.username}, expires_delta=access_token_expires
     )
     return Token(access_token=access_token, token_type="bearer")
+
+
+@router.post("/users", status_code=status.HTTP_201_CREATED)
+def create_user(user_create: UserSchema, db: Session = Depends(get_db)):
+    hashed_password = password.hash(user_create.password)
+    user_create.password = hashed_password
+
+    new_user = User(**user_create.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
