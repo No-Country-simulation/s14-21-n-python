b49434776c9cac6d4e1fdf0562823b11988d12ad
Ignacio Cornejo <106226900+IgnacioCornejo@users.noreply.github.com>
2024-04-13 19:38:17 -0300

    mapeo de json agregado

--- frontend/src/components/Supplier/Supplier.jsx
+++ frontend/src/components/Supplier/Supplier.jsx
@@ -1,38 +1,9 @@
 import style from "./Supplier.module.css";
 import AddSupplier from "../AddSupplier/AddSupplier";
+import jsonData from "./suppliers.json";
 
 const Supplier = () => {
-  const testArray = [
-    {
-      id: "1",
-      name: "Proveedor 1",
-      address: "direccion 1",
-      tel: "1144343443",
-      mail: "proveedor1@gmail.com",
-    },
-    {
-      id: "2",
-      name: "Proveedor 2",
-      address: "direccion 2",
-      tel: "1144343444",
-      mail: "proveedor2@gmail.com",
-    },
-    {
-      id: "3",
-      name: "Proveedor 3",
-      address: "direccion 3",
-      tel: "1144343445",
-      mail: "proveedor3@gmail.com",
-    },
-    {
-      id: "4",
-      name: "Proveedor 4",
-      address: "direccion 4",
-      tel: "1144343446",
-      mail: "proveedor4@gmail.com",
-    },
-  ];
-
+  
   return (
     <main className={style.container}>
       <div className={style.title}>
@@ -51,7 +22,7 @@ const Supplier = () => {
           <div className={style.column}>Telefono</div>
           <div className={style.column}>Dirección</div>
         </div>
-        {testArray.map((prov) => {
+        {jsonData.map((prov) => {
           return (
             <div className={style.card} key={prov.id}>
               <p className={style.column}>{prov.name}</p>
