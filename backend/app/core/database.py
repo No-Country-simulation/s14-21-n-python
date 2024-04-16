5bb46592f3d34ea2ef0e928ae1b6899ee6d68639
suspiciousRaccoon <spablocabral@gmail.com>
2024-04-15 22:37:57 -0300

    chore: add autocommit False

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,6 +1,6 @@
 from core.config import settings
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{settings['database_username']}:{settings['database_password']}@{settings['database_host']}:{settings['database_port']}/{settings['database_name']}"
 
@@ -9,6 +9,7 @@ async_engine = create_async_engine(url=SQLALCHEMY_DATABASE_URL)
 AsyncSessionLocal = async_sessionmaker(
     bind=async_engine,
     autoflush=False,
+    autocommit=False,
     future=True,
 )
 
