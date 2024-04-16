88f24f0563b0a55f98975b2140cc056729ae214d
Nahuel <nahuel.94.arrieta@gmail.com>
2024-04-16 09:58:14 -0300

    feat(api/main.py): add router auth and user

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from api.routes import hello, auth
+from api.routes import hello, auth, user
 
 api_router = APIRouter()
 api_router.include_router(hello.router)
 
 api_router.include_router(auth.router)
+api_router.include_router(user.router)
