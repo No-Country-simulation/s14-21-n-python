c878e2ec67620d80aae8760170812d534688adf0
suspiciousRaccoon <spablocabral@gmail.com>
2024-04-15 21:36:52 -0300

    refactor(core/database.py): use async engine instead of sync

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,11 +1,16 @@
 from core.config import settings
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{settings['database_username']}:{settings['database_password']}@{settings['database_host']}:{settings['database_port']}/{settings['database_name']}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+async_engine = create_async_engine(url=SQLALCHEMY_DATABASE_URL)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=async_engine,
+    autoflush=False,
+    future=True,
+)
 
 
 class Base(DeclarativeBase):
