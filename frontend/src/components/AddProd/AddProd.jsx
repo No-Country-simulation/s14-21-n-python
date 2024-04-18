b09502f015799be3780678823a3083dcb1c3da41
Scarbonell11 <chebastroto@gmail.com>
2024-04-17 22:06:19 -0300

    fix: validado formulario AddProd

--- frontend/src/components/AddProd/AddProd.jsx
+++ frontend/src/components/AddProd/AddProd.jsx
@@ -1,19 +1,52 @@
 import style from "./AddProd.module.css";
+import { useState } from "react";
 
 const AddProd = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+    category: "",
+    brand: "",
+    provider: "",
+    amount: "",
+    price: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
   return (
     <form className={style.container}>
       <div className={style.inputs}>
         <label>Nombre</label>
-        <input type="text" />
+        <input
+          type="text"
+          required
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
       </div>
       <div className={style.inputs}>
         <label>Descripción</label>
-        <input type="text" />
+        <input
+          type="text"
+          required
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+        />
       </div>
       <div className={style.inputs}>
         <label>Categoría</label>
-        <select name="Categorias" id="cat">
+        <select
+          name="Categoria"
+          id="categoria"
+          required
+          value={formData.category}
+          onChange={handleChange}
+        >
           <option value="categoria1">Categoría 1</option>
           <option value="categoria2">Categoría 2</option>
           <option value="categoria3">Categoría 3</option>
@@ -26,19 +59,50 @@ const AddProd = () => {
       </div>
       <div className={style.inputs}>
         <label>Marca</label>
-        <input type="text" />
+        <input
+          type="text"
+          required
+          name="brand"
+          value={formData.brand}
+          onChange={handleChange}
+        />
       </div>
       <div className={style.inputs}>
         <label>Proveedor</label>
-        <input type="text" />
+        <input
+          type="text"
+          required
+          name="provider"
+          value={formData.provider}
+          onChange={handleChange}
+        />
       </div>
       <div className={style.inputs}>
         <label>Cantidad</label>
-        <input type="text" />
+        <input
+          type="number"
+          inputMode="numeric"
+          min="0"
+          pattern="[0-9]*"
+          required
+          name="amount"
+          value={formData.amount}
+          onChange={handleChange}
+        />
       </div>
       <div className={style.inputs}>
         <label>Precio</label>
-        <input type="text" />
+        <input
+          type="number"
+          inputMode="numeric"
+          min="0"
+          pattern="[0-9]+(\.[0-9]{1,2})?"
+          step="0.01"
+          required
+          name="price"
+          value={formData.price}
+          onChange={handleChange}
+        />
       </div>
 
       <input className={style.button} type="submit" value="Agregar" />
