f81bcf98c5dd67fda4a6a53eac374e8d94691bdc
Nahuel <nahuel.94.arrieta@gmail.com>
2024-04-17 21:45:29 -0300

    refactor(routes/user): refactoring routes users

--- backend/app/api/routes/user.py
+++ backend/app/api/routes/user.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, status
-from schemas import UserSchema, UserUpdate
+from schemas import UserSchema, UserUpdate, UserOut
 from utils.password import hash
 from api.dependencies.db import get_session
 
@@ -8,11 +8,7 @@ from api.dependencies.auth import validate_authenticate_user
 from sqlalchemy.ext.asyncio.session import AsyncSession
 from crud.user import UserCrud
 
-router = APIRouter(
-    prefix="/api/users",
-    tags=["users"],
-    responses={404: {"description": "Not found"}},
-)
+router = APIRouter()
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
@@ -26,37 +22,48 @@ async def create_user(user_create: UserSchema, db: AsyncSession = Depends(get_se
     return new_user
 
 
-@router.get(
-    "/{user_id}/",
-    status_code=status.HTTP_200_OK,
-    dependencies=[Depends(validate_authenticate_user)],
-)
-async def get_user_id(user_id: int, db: AsyncSession = Depends(get_session)):
+@router.get("/{user_id}/", status_code=status.HTTP_200_OK)
+async def get_user_id(
+    user_id: int,
+    db: AsyncSession = Depends(get_session),
+    current_user: str = Depends(validate_authenticate_user),
+):
 
     user = await UserCrud(db).get(user_id)
 
     return user
 
 
-@router.get("/", status_code=status.HTTP_200_OK)
-async def get_users(db: AsyncSession = Depends(get_session)):
-    pass
+@router.get(
+    "/",
+    status_code=status.HTTP_200_OK,
+)
+async def get_all_users(
+    db: AsyncSession = Depends(get_session),
+    current_user: str = Depends(validate_authenticate_user),
+):
+    users = await UserCrud(db).get_all()
+
+    return users
 
 
 @router.delete("/{user_id}/", status_code=status.HTTP_200_OK)
-async def delete_user(user_id: int, db: AsyncSession = Depends(get_session)):
+async def delete_user(
+    user_id: int,
+    db: AsyncSession = Depends(get_session),
+    current_user: str = Depends(validate_authenticate_user),
+):
 
     await UserCrud(db).delete(user_id)
-
-    return {}
 
 
 @router.put("/{user_id}/", status_code=status.HTTP_200_OK)
 async def update_user(
-    user_id: int, update_user: UserUpdate, db: AsyncSession = Depends(get_session)
+    user_id: int,
+    update_user: UserUpdate,
+    db: AsyncSession = Depends(get_session),
+    current_user: str = Depends(validate_authenticate_user),
 ):
 
     user = await UserCrud(db).update(user_id, update_user)
-    print(f"{user=}")
-
     return user
