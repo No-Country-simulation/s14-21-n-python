8c03a3b9633abfee621c5f0893c15881df513693
Nahuel <nahuel.94.arrieta@gmail.com>
2024-04-17 21:29:13 -0300

    feacture(api/main): Add prefix to auth and users

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -4,5 +4,15 @@ from api.routes import hello, auth, user
 api_router = APIRouter()
 api_router.include_router(hello.router)
 
-api_router.include_router(auth.router)
-api_router.include_router(user.router)
+api_router.include_router(
+    auth.router,
+    prefix="/api/auth",
+    tags=["Authentication"],
+    responses={404: {"description": "Not found"}},
+)
+api_router.include_router(
+    user.router,
+    prefix="/api/users",
+    tags=["Users"],
+    responses={404: {"description": "Not found"}},
+)
