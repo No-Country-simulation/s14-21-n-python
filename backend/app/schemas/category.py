58e863743e0b0decadc5d15d0c1adc554f6a2789
Nahuel <nahuel.94.arrieta@gmail.com>
2024-04-20 00:01:24 -0300

    feacture(schemas/category): add create and update schemas category

--- backend/app/schemas/category.py
+++ backend/app/schemas/category.py
@@ -3,7 +3,17 @@ from pydantic import BaseModel, ConfigDict
 
 class CategorySchema(BaseModel):
     id: int
+class CreateCategory(BaseModel):
+    business_id: int | None = None
+    product_id: int | None = None
     name: str
     description: str | None
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class UpdateCategory(BaseModel):
+    business_id: int | None = None
+    product_id: int | None = None
+    name: str | None = None
+    description: str | None
