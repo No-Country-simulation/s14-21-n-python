a834a1638ee8e0bb2330ac1ad7bd006dba8d9f6e
suspiciousRaccoon <spablocabral@gmail.com>
2024-04-20 17:16:31 -0300

    fix(shcemas/category.py): remove product_id from category schema

--- backend/app/schemas/category.py
+++ backend/app/schemas/category.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel, ConfigDict
 class CategorySchema(BaseModel):
     id: int
     business_id: int | None = None
-    product_id: int | None = None
     name: str
     description: str | None = None
 
@@ -13,7 +12,6 @@ class CategorySchema(BaseModel):
 
 class CreateCategory(BaseModel):
     business_id: int | None = None
-    product_id: int | None = None
     name: str
     description: str | None
 
@@ -22,6 +20,5 @@ class CreateCategory(BaseModel):
 
 class UpdateCategory(BaseModel):
     business_id: int | None = None
-    product_id: int | None = None
     name: str | None = None
     description: str | None
