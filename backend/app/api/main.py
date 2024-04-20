1f8233ad02661e55c2de5fb4bffb6fa8497bc3a6
Nahuel <nahuel.94.arrieta@gmail.com>
2024-04-20 00:00:10 -0300

    feacture(api/main): add api_router category

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from api.routes import hello, auth, user
+from api.routes import hello, auth, user, category
 
 api_router = APIRouter()
 api_router.include_router(hello.router)
@@ -14,5 +14,11 @@ api_router.include_router(
     user.router,
     prefix="/api/users",
     tags=["Users"],
+    responses={404: {"description": "Not found"}},
+)
+api_router.include_router(
+    category.router,
+    prefix="/api/categories",
+    tags=["Category"],
     responses={404: {"description": "Not found"}},
 )
