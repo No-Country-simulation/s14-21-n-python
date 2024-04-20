97fef80256a133fff394c580d270da9121c5ce4e
Nahuel <nahuel.94.arrieta@gmail.com>
2024-04-20 01:25:18 -0300

    feacture(schemas/product): add create and update schemas products

--- backend/app/schemas/product.py
+++ backend/app/schemas/product.py
@@ -18,3 +18,19 @@ class ProductSchema(BaseModel):
     expiration_date: datetime | None = None
 
     model_config = ConfigDict(from_attributes=True)
+class CreateProductSchema(BaseModel):
+    business_id: int | None = None
+    brand: str
+    name: str
+    stock: int
+    description: str | None = None
+    minimum_stock: int | None = None
+    original_price: Decimal
+    purchase_date: datetime | None = None
+    expiration_date: datetime | None = None
+
+
+class UpdateProduct(BaseModel):
+    stock: int | None = None
+    description: str | None = None
+    minimum_stock: int | None = None
