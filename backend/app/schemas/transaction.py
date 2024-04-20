4e5fcbd915ae61e2c924bcec0eebe62312b963cd
Nahuel <nahuel.94.arrieta@gmail.com>
2024-04-19 23:24:25 -0300

    feacture(schemas/transaction): add create and update  schemas transaction

--- backend/app/schemas/transaction.py
+++ backend/app/schemas/transaction.py
@@ -19,3 +19,27 @@ class TransactionSchema(BaseModel):
     transaction_date: datetime
 
     model_config = ConfigDict(from_attributes=True)
+class CreateTransaction(BaseModel):
+    business_id: int | None = None
+    product_id: int | None = None
+    client_id: Optional[int]
+    supplier_id: int | None = None
+    type_op: TypeOpEnum
+    quantity: int
+    price: Decimal | None = None
+    payment_method: PaymentMethodEnum
+    status: StatusEnum
+    transaction_date: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UpdateTransaction(BaseModel):
+    type_op: TypeOpEnum | None = None
+    quantity: int | None = None
+    price: Decimal | None = None
+    payment_method: PaymentMethodEnum | None = None
+    status: StatusEnum | None = None
+    transaction_date: datetime | None = None
+
+    model_config = ConfigDict(from_attributes=True)
