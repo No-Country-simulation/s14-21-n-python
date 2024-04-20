b428909087bef1cb34ce27dc19d824d4a8b31a18
suspiciousRaccoon <spablocabral@gmail.com>
2024-04-20 16:16:03 -0300

    refactor(routes/transaction.py): add path parameter business_id followed by endpoint name

--- backend/app/api/routes/transaction.py
+++ backend/app/api/routes/transaction.py
@@ -1,16 +1,23 @@
 from typing import List
+
 from api.dependencies.auth import validate_authenticate_user
 from api.dependencies.db import get_session
 from crud.transaction import TransactionCrud
 from fastapi import APIRouter, Depends, status
-from schemas.transaction import TransactionSchema, CreateTransaction, UpdateTransaction
+from schemas.transaction import CreateTransaction, TransactionSchema, UpdateTransaction
+from schemas.transaction import CreateTransaction, TransactionSchema, UpdateTransaction
 from sqlalchemy.ext.asyncio.session import AsyncSession
 
 router = APIRouter()
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=TransactionSchema)
+@router.post(
+    "/{business_id}/transactions/",
+    status_code=status.HTTP_201_CREATED,
+    response_model=TransactionSchema,
+)
 async def create_transaction(
+    business_id: int,
     transaction_create: CreateTransaction,
     db: AsyncSession = Depends(get_session),
     current_user: str = Depends(validate_authenticate_user),
@@ -21,11 +28,12 @@ async def create_transaction(
 
 
 @router.get(
-    "/{transaction_id}/",
+    "/{business_id}/transactions/{transaction_id}/",
     status_code=status.HTTP_200_OK,
     response_model=TransactionSchema,
 )
 async def get_transaction_id(
+    business_id: int,
     transaction_id: int,
     db: AsyncSession = Depends(get_session),
     current_user: str = Depends(validate_authenticate_user),
@@ -36,11 +44,12 @@ async def get_transaction_id(
 
 
 @router.get(
-    "/",
+    "/{business_id}/transactions/",
     status_code=status.HTTP_200_OK,
     response_model=List[TransactionSchema],
 )
 async def get_all_transactions(
+    business_id: int,
     db: AsyncSession = Depends(get_session),
     current_user: str = Depends(validate_authenticate_user),
 ):
@@ -49,8 +58,11 @@ async def get_all_transactions(
     return transactions
 
 
-@router.delete("/{transaction_id}/", status_code=status.HTTP_200_OK)
+@router.delete(
+    "/{business_id}/transactions/{transaction_id}/", status_code=status.HTTP_200_OK
+)
 async def delete_transaction(
+    business_id: int,
     transaction_id: int,
     db: AsyncSession = Depends(get_session),
     current_user: str = Depends(validate_authenticate_user),
@@ -58,14 +70,16 @@ async def delete_transaction(
     await TransactionCrud(db).delete(transaction_id)
 
 
-@router.put("/{transaction_id}/", status_code=status.HTTP_200_OK)
+@router.put(
+    "/{business_id}/transactions/{transaction_id}/", status_code=status.HTTP_200_OK
+)
 async def update_transaction(
+    business_id: int,
     transaction_id: int,
     update_transaction: UpdateTransaction,
     db: AsyncSession = Depends(get_session),
     current_user: str = Depends(validate_authenticate_user),
 ):
-
     transaction = await TransactionCrud(db).update(transaction_id, update_transaction)
 
     return transaction
