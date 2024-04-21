460d64e58b2417486328423d455176396141326c
suspiciousRaccoon <spablocabral@gmail.com>
2024-04-21 16:07:27 -0300

    chore: update cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,24 +1,19 @@
-from fastapi import FastAPI
-from fastapi.middelware.cors import CORSMiddleware
 from api.main import api_router
-
+from core.config import settings
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Configuaracion de CORS
-# Lista de origenes que pueden tener acceso
-""" origins = [
-    "http://localhost:8080",
-    "http://localhost:5173",
-    "http://localhost:5501",
-] """
 
+# CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["*"],   # (*) Comodin que acepta cualquier solicitud desde cualquier origen
-    allow_credentials = True,   # Acepta cookies, tokens, etc.
-    allows_methods = ["GET", "POST", "PUT", "DELETE"],   # Métodos HTTP permitidos
-    allows_headers = ["*"]   # Cabeceras permitidas
+    # domains must be specified in the .env as a string with a space as delimiter
+    allow_origins=[settings["cors_domains"].split(" ")],
+    allow_credentials=True,
+    allows_methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allows_headers=["*"],
 )
 
 # Include API routes
